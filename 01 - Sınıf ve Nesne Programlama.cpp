0e77a845765730717c707050d31df9dc0860fa3f
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-02-02 16:42:47 +0300

    Rename 01 - İlk Class'ımızı Oluşturalım.cpp to 01 - Sınıf ve Nesne Programlama.cpp

---
+++ 01 - Sınıf ve Nesne Programlama.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Nokta{ 	// Nokta adında bir sınıf oluşturduk.
+	public:		// ilgili sınıfın public olduğunu belirttik. : ile bittiğine dikkat ediniz.
+	int x,y; 	// int tipinde x ve y oluşturduk
+};
+
+
+int main() {
+	
+	Nokta n;	// Nokta sınıfına ait "n" adında bir NESNE oluşturduk.
+	n.x=5;		// n.x diyerek "n" NESNESİNİN "x" elemanına değer verdik.
+	n.y=10;		// n.y diyerek "n" NESNESİNİN "y" elemanına değer verdik.
+	
+	cout << n.x << "," << n.y << endl;	// "n" Nesnesinin x ve y elemanlarını ekrana yazdızdık.
+	
+	return 0;
+}
